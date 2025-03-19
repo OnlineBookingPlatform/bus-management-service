141f9f578e2e91902c9cab0d665cd6312c16def3
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-03-19 11:51:23 +0700

    init connect port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 4002;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: PORT,
+      },
+    },
+  );
+
+  await app.listen();
+  console.log(`✅ Account Service is listening on port ${PORT}`);
 }
 bootstrap();
